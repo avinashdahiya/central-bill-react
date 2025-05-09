5e7c5d7e3af8de977a87894bea05a28366aa4057
avinashdahiya <addahiya2002@gmail.com>
2025-05-09 14:39:48 +0530

    make responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,36 +8,45 @@ import CustomContent from './common/CustomContent';
 
 const Header = () => {
     return (
-        <section className='bg-[url(./assets/images/png/blue-img.png)] bg-cover bg-center lg:h-[810px] md:rounded-bl-[100px] lg:rounded-bl-[200px] h-none'>
+        <section className='relative bg-[url(./assets/images/png/blue-img.png)] bg-cover bg-center md:rounded-bl-[100px] lg:rounded-bl-[200px]'>
             <Navbar />
-            <div className='container max-w-[1140px] mx-auto flex flex-col items-center justify-center p-4'>
-                <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
-                    <div className='flex flex-col mt-[40px] lg:mt-[122px]'>
-                        <h1 className='text-[40px] sm:text-[50px] lg:text-[60px] font-normal text-white leading-[110%] md:max-w-[506px]'>
+            <div className='container max-w-[1140px] mx-auto flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8 py-8 lg:py-16'>
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-8 items-center'>
+                    {/* Left Content */}
+                    <div className='flex flex-col mt-6 sm:mt-10 lg:mt-20'>
+                        <h1 className='text-[32px] sm:text-[42px] lg:text-[60px] font-normal text-white leading-[110%]'>
                             Easily Compare <span className='font-bold'>Energy, Gas, And Internet</span> Plans
                         </h1>
                         <CustomContent
                             content="Find and switch to the best energy, gas, and internet plans with ease. Our platform simplifies your search, letting you compare providers and make informed decisions, all in one place."
                             className="text-white mt-4 opacity-90"
                         />
-                        <label className="relative flex items-center mt-4 lg:mt-[42px] max-w-[476px]">
+                        <label className="relative flex items-center mt-5 max-w-full sm:max-w-[480px]">
                             <input
                                 type="text"
                                 id="text"
                                 placeholder="Start typing your address"
-                                className="w-[476px] px-[22px] py-[24px] rounded-[0px_100px_100px_20px] border-0 text-gray-500 text-base font-normal leading-[17.6px] bg-white focus:outline-none focus:ring-2 focus:ring-[#00A8E8]"
+                                className="w-full px-6 py-5 rounded-[0px_100px_100px_20px] border-0 text-gray-500 text-base bg-white focus:outline-none focus:ring-2 focus:ring-[#00A8E8]"
                             />
                             <Custombtn
                                 btn="Compare"
-                                className="absolute top-1/2 -translate-y-1/2 right-[10px] text-white font-bold bg-[#00A8E8] hover:bg-[#006D8F] hover:text-white border-2 border-[#00A8E8] transition-all duration-300 transform hover:scale-105"
+                                className="absolute top-1/2 -translate-y-1/2 right-2 text-white font-bold bg-[#00A8E8] hover:bg-[#006D8F] border-2 border-[#00A8E8] transition-all duration-300 transform hover:scale-105 px-6 py-2 rounded-full"
                             />
                         </label>
                     </div>
-                    <div className='relative z-20'>
-                        <img src={girl} alt="Header Image" className='w-full relative z-3 mt-[30px] lg:mt-[109px]' />
+
+                    {/* Right Image */}
+                    <div className='relative z-20 mt-8 md:mt-0'>
+                        <img src={girl} alt="Header" className='w-full max-w-full h-auto' />
                     </div>
-                    <img src={dots} alt="Header Image" className='absolute right-[120px] mt-[53px] hidden xl:block z-0' />
                 </div>
+
+                {/* Dots Background */}
+                <img
+                    src={dots}
+                    alt="Decoration Dots"
+                    className='absolute right-6 xl:right-[120px] top-[50%] transform -translate-y-1/2 hidden xl:block z-0'
+                />
             </div>
         </section>
     );
