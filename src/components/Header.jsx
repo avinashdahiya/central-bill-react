98a54b59a1fe855b3e8690e790eb03fa0dbf828b
avinashdahiya <addahiya2002@gmail.com>
2025-05-10 11:49:59 +0530

    all done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,47 +1,66 @@
-import React, { useEffect} from 'react'
-import Navbar from './Navbar'
-import girl from '../assets/images/png/girl.png'
-import dots from '../assets/images/svg/dots.svg'
-import Custombtn from './common/Custombtn'
-import AOS from 'aos';
-import 'aos/dist/aos.css'
-import CustomContent from './common/CustomContent'
-import CustomInput from './common/CustomInput'
+import React from 'react';
+import Navbar from './Navbar';
+import girl from '../assets/images/png/girl.png';
+import dots from '../assets/images/svg/dots.svg';
+import Custombtn from './common/Custombtn';
+import CustomContent from './common/CustomContent';
+import CustomInput from './common/CustomInput';
 
 const Header = () => {
-    useEffect(() => {
-        AOS.init({
-            duration: 2000, 
-            once: true,     
-        });
-    }, [])
     return (
-        <section className='bg-[url(./assets/images/png/blue-img.png)] bg-cover bg-center lg:h-[810px] md:rounded-bl-[100px] lg:rounded-bl-[200px] h-none p-4'>
+        <section className='bg-[url(./assets/images/png/blue-img.png)] bg-cover bg-center lg:h-[810px] md:rounded-bl-[100px] lg:rounded-bl-[200px] p-4'>
             <Navbar />
-            <div className='conatiner max-w-[1140px] mx-auto flex flex-col items-center justify-center  '>
-                <div className=' grid grid-cols-1 md:grid-cols-2 gap-6'>
-                    <div className='flex flex-col  mt-[40px]  lg:mt-[122px]  mx-auto '>
-                        <h1 className='text-[35px] sm:text-[50px] lg:text-[60px] font-normal text-white leading-[110%] md:max-w-[506px]'>Easily Compare <span className='font-bold'>Energy, Gas, And Internet</span> Plans</h1>
-                        <CustomContent content="Find and switch to the best energy, gas, and internet plans with ease. Our platform simplifies your search, letting you compare providers and make informed decisions, all in one place." className="text-white mt-4 opacity-90"/>
+            <div className='container max-w-[1140px] mx-auto flex flex-col items-center justify-center'>
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+                    <div className='flex flex-col mt-[40px] lg:mt-[122px] mx-auto'>
+                        <h1 className='text-[35px] sm:text-[50px] lg:text-[60px] font-normal text-white leading-[110%] md:max-w-[506px]'>
+                            Easily Compare <span className='font-bold'>Energy, Gas, And Internet</span> Plans
+                        </h1>
+                        <CustomContent
+                            content="Find and switch to the best energy, gas, and internet plans with ease. Our platform simplifies your search, letting you compare providers and make informed decisions, all in one place."
+                            className="text-white mt-4 opacity-90"
+                        />
                         <div className="flex items-center relative max-w-[476px] mt-[30px] md:mt-[42px]">
-                            <CustomInput type="text" placeholder="Start typing your address" inputClass="pr-[160px]" />
-                            <Custombtn btnClass="absolute z-2 right-[10px]" btnText="Compare" whitesvg={<svg width="15" height="16" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M1 14.3691L14 1.36914M14 1.36914V13.8491M14 1.36914H1.52" stroke="white" stroke-width="1.93338" stroke-linecap="round" stroke-linejoin="round" />
-                            </svg>
-                            } />
+                            <CustomInput
+                                type="text"
+                                placeholder="Start typing your address"
+                                inputClass="pr-[160px]"
+                            />
+                            <Custombtn
+                                btnClass="absolute z-2 right-[10px]"
+                                btnText="Compare"
+                                whitesvg={
+                                    <svg width="15" height="16" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M1 14.3691L14 1.36914M14 1.36914V13.8491M14 1.36914H1.52"
+                                            stroke="white"
+                                            strokeWidth="1.93338"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                        />
+                                    </svg>
+                                }
+                            />
                         </div>
-
                     </div>
-                    <div data-aos="zoom-in-up" className='relative z-20'>
-                        <img src={girl} alt="Header Image" className='w-full  relative z-3 mt-[30px] lg:mt-[109px]  max-w-[558px] mx-auto pointer-events-none  ' /></div>
-                    <img src={dots} alt="Header Image" className=' absolute right-[120px] mt-[53px] hidden xl:block z-0 ' />
 
+                    <div className='relative z-20'>
+                        <img
+                            src={girl}
+                            alt="Header Girl"
+                            className='w-full relative z-3 mt-[30px] lg:mt-[109px] max-w-[558px] mx-auto pointer-events-none'
+                        />
+                    </div>
 
+                    <img
+                        src={dots}
+                        alt="Decorative Dots"
+                        className='absolute right-[120px] mt-[53px] hidden xl:block z-0'
+                    />
                 </div>
             </div>
-
         </section>
-    )
-}
+    );
+};
 
 export default Header;
--- src/components/SwitchEngery.jsx
+++ src/components/SwitchEngery.jsx
@@ -8,19 +8,19 @@ const SwitchEnergy = () => {
     return (
         <section className='bg-white' id="about">
             <div className='container max-w-[1140px] mx-auto flex flex-col items-center justify-center p-4'>
-                <div className='grid grid-cols-1 lg:grid-cols-2 gap-[75px]'>
-                    <div>
-                        <img src={light} className='mx-auto' alt="Energy Light" />
+                <div className='grid grid-cols-1 lg:grid-cols-2 gap-[40px] md:gap-[60px] lg:gap-[75px]'>
+                    <div className='flex justify-center'>
+                        <img src={light} className='mx-auto w-full max-w-[500px] md:max-w-[600px] lg:max-w-[700px]' alt="Energy Light" />
                     </div>
                     <div className='flex flex-col'>
-                        <h2 className='text-[38px] md:text-[48px] text-[#1E1E1E] leading-[110%] lg:max-w-[409px] capitalize font-bold'>
+                        <h2 className='text-[28px] sm:text-[38px] md:text-[48px] text-[#1E1E1E] leading-[110%] font-bold capitalize'>
                             Switching Energy <span className='font-normal'>Made Simple</span>
                         </h2>
                         <CustomContent
                             content="Say goodbye to confusing energy plans! We simplify the process by providing clear and competitive electricity and gas recommendations. Whether you're moving or just seeking a better deal, our platform connects you with the best energy providers quickly and easily, helping you save on energy bills."
                             className="text-[#00171F] opacity-90 mt-4 lg:max-w-[558px]"
                         />
-                        <p className='text-[24px] leading-[110%] font-normal text-[#003459] mt-[38px] capitalize'>
+                        <p className='text-[20px] sm:text-[24px] leading-[110%] font-normal text-[#003459] mt-[30px] capitalize'>
                             Benefits of Comparing Energy Plans
                         </p>
                         <ul className='list-disc mt-4 ml-7'>
@@ -28,9 +28,15 @@ const SwitchEnergy = () => {
                             <li className='font-normal text-base leading-[150%] text-[#00171F] mt-2'>Get better customer service.</li>
                             <li className='font-normal text-base leading-[150%] text-[#00171F] mt-2'>Explore greener, more sustainable energy options.</li>
                         </ul>
-                       <Custombtn btnClass="max-w-[210px] mt-[42px]" btnText="Compare With Us" whitesvg={<svg width="15" height="16" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M1 14.3691L14 1.36914M14 1.36914V13.8491M14 1.36914H1.52" stroke="white" stroke-width="1.93338" stroke-linecap="round" stroke-linejoin="round" />
-                        </svg>} />
+                        <Custombtn 
+                            btnClass="max-w-[210px] mt-[42px] self-center md:self-start" 
+                            btnText="Compare With Us" 
+                            whitesvg={
+                                <svg width="15" height="16" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M1 14.3691L14 1.36914M14 1.36914V13.8491M14 1.36914H1.52" stroke="white" strokeWidth="1.93338" strokeLinecap="round" strokeLinejoin="round" />
+                                </svg>
+                            } 
+                        />
                     </div>
                 </div>
             </div>
@@ -39,4 +45,3 @@ const SwitchEnergy = () => {
 };
 
 export default SwitchEnergy;
-
